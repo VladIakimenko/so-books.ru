8ebc4e4f5cf32e576a7574241be5c6a2dc6792af
GroofiP <varchikcreed@mail.ru>
2023-03-25 02:36:47 +0300

    [HERMES-11] add cors and allowed

--- hermes/settings/production.py
+++ hermes/settings/production.py
@@ -1,3 +1,4 @@
 DEBUG = False
-ALLOWED_HOSTS = ['80.78.246.159', "home-sobolev.ru", "hermes.home-sobolev.ru"]
-CSRF_TRUSTED_ORIGINS = ['https://home-sobolev.ru', 'https://hermes.home-sobolev.ru', 'https://80.78.246.159']
+ALLOWED_HOSTS = ['80.78.246.159', "home-sobolev.ru", "hermes.home-sobolev.ru", "http://213.189.201.49/"]
+CSRF_TRUSTED_ORIGINS = ['https://home-sobolev.ru', 'https://hermes.home-sobolev.ru', 'https://80.78.246.159',
+                        "http://213.189.201.49/"]
