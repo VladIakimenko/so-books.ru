365b6f9f2e67f9d4423195ce2a3a0f363793afa6
GroofiP <varchikcreed@mail.ru>
2023-03-26 23:35:21 +0300

    [HERMES-14] fixed cors and allowed and csrf

--- hermes/settings/production.py
+++ hermes/settings/production.py
@@ -3,3 +3,10 @@ ALLOWED_HOSTS = ['80.78.246.159', "213.189.201.49", "home-sobolev.ru", "hermes.h
 CSRF_TRUSTED_ORIGINS = ['https://home-sobolev.ru', 'http://home-sobolev.ru',
                         'https://hermes.home-sobolev.ru', 'http://hermes.home-sobolev.ru',
                         'http://80.78.246.159', "http://213.189.201.49"]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://home-sobolev.ru', 'http://home-sobolev.ru',
+    'https://hermes.home-sobolev.ru', 'http://hermes.home-sobolev.ru',
+    'http://80.78.246.159', "http://213.189.201.49",
+    'http://localhost:8000', "http://localhost:8080"
+]
