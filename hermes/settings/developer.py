787803669a0f9e45f0d0a111d5b7ae34847d0f65
VladIakimenko <v_yakimenko@inbox.ru>
2023-05-11 12:35:38 +0400

    [HERMES-24] Downing container before re-deployment

--- hermes/settings/developer.py
+++ hermes/settings/developer.py
@@ -1,9 +1,4 @@
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
+CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:9000",
-    "http://127.0.0.1",
-]
