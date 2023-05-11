687664a53cc0818049d186fd1668302a53713f9b
Vladimir Iakimenko <v_yakimenko@inbox.ru>
2023-05-11 05:46:40 +0000

    [DEVELOPMENT] 'Fixed allowed hosts for dev'

--- hermes/settings/__init__.py
+++ hermes/settings/__init__.py
@@ -2,7 +2,7 @@ import socket
 from .base import *
 
 ip = socket.gethostbyname(socket.gethostname())
-ip_hosts = ['89.108.98.87', '80.78.246.159']
+ip_hosts = ['80.78.246.159']
 
 if ip in ip_hosts:
     from .production import *
