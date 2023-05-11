d19e2e3b7c690204d10c44f0f8eacc38b3d737e3
Vladimir Iakimenko <v_yakimenko@inbox.ru>
2023-05-11 07:32:58 +0000

    [DEVELOPMENT] CORS allowed for dev

--- hermes/settings/developer.py
+++ hermes/settings/developer.py
@@ -1,9 +1,3 @@
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
-
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:9000",
-    "http://127.0.0.1",
-]
+CORS_ALLOWED_ORIGINS = ["*"]
